42e22cd503204b893644b49de27d12bb3b6e2f6c
Nino Termulo <ninotermulo01@gmail.com>
2022-07-04 21:09:41 +0800

    Add loop that gets user input again while value is not ROCK, PAPER or SCISSOR

--- rules.js
+++ rules.js
@@ -1,5 +1,12 @@
 // === get input from user ===
+
 let userInput = prompt("Pick between rock, paper or scissor");
 let userPlay = userInput.toUpperCase(0);
 
+// === prevent user from placing invalid answer ===
 
+// while userPlay is not ROCK, PAPER OR SCISSOR
+while (!(userPlay === "ROCK" || userPlay === "PAPER" || userPlay === "SCISSOR"  )) {
+    userInput = prompt("Please type a valid answer");
+    userPlay = userInput.toUpperCase(0);
+}
