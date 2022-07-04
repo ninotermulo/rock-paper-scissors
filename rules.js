50a25a188105fdb0b8dba25b57f07ca2c2428337
Nino Termulo <ninotermulo01@gmail.com>
2022-07-04 21:51:52 +0800

    Add function that plays a single round of game

--- rules.js
+++ rules.js
@@ -23,3 +23,33 @@ function computerPlay() {
         return "SCISSOR";
     }
 }
+
+// === Create a function that plays one round of game ===
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
+        // evaluates first if user and computer has same choice
+        case playerSelection === computerSelection:
+            return "Tie!";
+            break;
+        case playerSelection === "ROCK":
+            if (computerSelection === "SCISSOR") { 
+                return "You win! Rock beats Scissor"
+            } else { // if computer selects paper
+                return "You lose! Paper beats Rock!"
+            }
+            break;
+        case playerSelection === "PAPER":
+            if (computerSelection === "ROCK") {
+                return "You win! Paper beats Rock"
+            } else { // if computer selects scissor
+                return "You lose! Scissor beats Paper!"
+            }
+            break;
+        default: // if player selects scissor
+            if (computerSelection === "SCISSOR") {
+                return "You win! Scissor beats Paper"
+            } else { // if computer selects rock
+                return "You lose! Rock beats Scissor!"
+            }                 
+    }
+}
