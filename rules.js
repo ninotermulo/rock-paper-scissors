a4babbbdbd8767d976bb8956f59db49803654aa5
Nino Termulo <ninotermulo01@gmail.com>
2022-07-04 22:20:35 +0800

    Create function for user input

--- rules.js
+++ rules.js
@@ -1,14 +1,16 @@
-// === get input from user ===
+// === create a function for user's choice ===
 
+function userPlay() {
+// === get input from user ===
 let userInput = prompt("Pick between rock, paper or scissor");
 let userPlay = userInput.toUpperCase(0);
-
 // === prevent user from placing invalid answer ===
-
 while (!(userPlay === "ROCK" || userPlay === "PAPER" || userPlay === "SCISSOR"  )) {
     userInput = prompt("Please type a valid answer");
     userPlay = userInput.toUpperCase(0);
 }
+}
+
 
 // === create function that randomly returns ROCK, PAPER, SCISSORS === 
 // this is the computer's choice
