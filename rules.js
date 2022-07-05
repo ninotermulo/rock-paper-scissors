2e77b3cd61880e33aea1f4e2744eec69156b1b2c
Nino Termulo <ninotermulo01@gmail.com>
2022-07-05 22:45:30 +0800

    Add line breaks to have less than 80 columns

--- rules.js
+++ rules.js
@@ -5,7 +5,8 @@ function userPlay() {
 let userInput = prompt("Pick between rock, paper or scissor");
 let userPlayVar = userInput.toUpperCase(0);
 // === prevent user from placing invalid answer ===
-while (!(userPlayVar === "ROCK" || userPlayVar === "PAPER" || userPlayVar === "SCISSOR")) {
+while (!(userPlayVar === "ROCK" || userPlayVar === "PAPER" || 
+    userPlayVar === "SCISSOR")) {
     userInput = prompt("Please type a valid answer");
     userPlayVar = userInput.toUpperCase(0);
 }
@@ -16,7 +17,8 @@ return userPlayVar;
 // this is the computer's choice
 
 function computerPlay() {
-    let computerInput = Math.floor(Math.random() * 3) + 1; // returns number from 1-3
+    let computerInput = Math.floor(Math.random() * 3) + 
+    1; // returns number from 1-3
     if (computerInput === 1) {
         return "ROCK";
     } else if (computerInput === 2) {
@@ -66,11 +68,13 @@ function game() {
     for (let i = 0; i < 5; i++) {
         roundResult = playRound(userPlay(), computerPlay());
         alert(roundResult);
-        // returns index position of first letter. If string can't be find, it returns -1
+        /*returns index position of first letter. If string can't be find, 
+        it returns -1*/
         let roundResultSearch = roundResult.search("You win!");
         // check if roundResult has a string of "Tie!"
         let roundResultTie = roundResult.search("Tie!");
-        (roundResultSearch === 0) ? userScore++ : // check if roundResulTie is 0. Index position of Y in "You win!"
+        // check if roundResulTie is 0. Index position of Y in "You win!"
+        (roundResultSearch === 0) ? userScore++ : 
             // checks if no "You win!" and "Tie" string
             (roundResultSearch < 0 && roundResultTie < 0) ? computerScore++ :
             userScore; // do nothing
