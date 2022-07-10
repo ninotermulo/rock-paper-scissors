8134d636d22cd52d92dddee9fdb5a37d1343c103
Nino Termulo <ninotermulo01@gmail.com>
2022-07-10 20:28:30 +0800

    Play game on click event

--- rules.js
+++ rules.js
@@ -1,12 +1,43 @@
 // === User selection on clicking button ===
 
-let playerSelection;
 const choices = document.querySelectorAll('button');
-choices.forEach(choice => choice.addEventListener('click', userPlay));
-function userPlay(e) {
-    const result = e.target.getAttribute("id");
-    playerSelection = result.toUpperCase();                                               
+choices.forEach(choice => choice.addEventListener('click', playRound));
+
+function playRound(e) {
+    const divResult = document.createElement('div');
+    document.body.appendChild(divResult);  
+    const buttonResult = e.target.getAttribute("id");
+
+    const playerSelection = buttonResult.toUpperCase();
+    const computerSelection = computerPlay();
+    switch (true) {
+        // evaluates first if user and computer has same choice
+        case playerSelection === computerSelection:
+            divResult.innerText = "Tie!";
+            break;
+        case playerSelection === "ROCK":
+            if (computerSelection === "SCISSOR") {
+                divResult.innerText = "You win! Rock beats Scissor";
+            } else { // if computer selects paper
+                divResult.innerText = "You lose! Paper beats Rock!";
+            }
+            break;
+        case playerSelection === "PAPER":
+            if (computerSelection === "ROCK") {
+                divResult.innerText = "You win! Paper beats Rock";
+            } else { // if computer selects scissor
+                divResult.innerText = "You lose! Scissor beats Paper!";
+            }
+            break;
+        default: // if player selects scissor
+            if (computerSelection === "PAPER") {
+                divResult.innerText = "You win! Scissor beats Paper";
+            } else { // if computer selects rock
+                divResult.innerText = "You lose! Rock beats Scissor!";
+            }             
+    }
 };
+
 
 // === create function that randomly returns ROCK, PAPER, SCISSORS === 
 // this is the computer's choice
@@ -23,37 +54,10 @@ function computerPlay() {
     }
 }
 
-// === Create a function that plays one round of game ===
 
-function playRound(playerSelection, computerSelection) {
-    const divResult = document.createElement('div');
-    switch (true) {
-        // evaluates first if user and computer has same choice
-        case playerSelection === computerSelection:
-            return "Tie!";
-            break;
-        case playerSelection === "ROCK":
-            if (computerSelection === "SCISSOR") { 
-                return "You win! Rock beats Scissor";
-            } else { // if computer selects paper
-                return "You lose! Paper beats Rock!";
-            }
-            break;
-        case playerSelection === "PAPER":
-            if (computerSelection === "ROCK") {
-                return "You win! Paper beats Rock";
-            } else { // if computer selects scissor
-                return "You lose! Scissor beats Paper!";
-            }
-            break;
-        default: // if player selects scissor
-            if (computerSelection === "PAPER") {
-                return "You win! Scissor beats Paper";
-            } else { // if computer selects rock
-                return "You lose! Rock beats Scissor!";
-            }                 
-    }
-}
+      
+
+
 
 
 
